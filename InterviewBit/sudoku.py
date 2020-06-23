0a04727987898dcf8e56f1be63c45df67358933c
Priyaranjan JC <priyaranjan780@gmail.com>
2020-06-23 10:05:00 -0500

    Interview Bit: sudoku
    Sudoku has only one unique solution and this is achieved using back tracking and hashmaps

--- InterviewBit/sudoku.py
+++ InterviewBit/sudoku.py
@@ -1,72 +1,98 @@
+from collections import defaultdict
 class Solution:
-    # @param A : list of list of chars
-    # @return nothing
-    def solveSudoku(self, A):
-
-        # check if the sudoku is valid in given state
-        def check_row(A, row):
-            list_row = [x for x in A[row] if x != '.']
-            set_row = set(list_row)
-            if len(list_row) == len(set_row):
-                return True
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        def could_place(d, row, col):
+            """
+            Check if one could place a number d in (row, col) cell
+            """
+            return not (d in rows[row] or d in columns[col] or \
+                    d in boxes[box_index(row, col)])
+        
+        def place_number(d, row, col):
+            """
+            Place a number d in (row, col) cell
+            """
+            rows[row][d] += 1
+            columns[col][d] += 1
+            boxes[box_index(row, col)][d] += 1
+            board[row][col] = str(d)
+            
+        def remove_number(d, row, col):
+            """
+            Remove a number which didn't lead 
+            to a solution
+            """
+            del rows[row][d]
+            del columns[col][d]
+            del boxes[box_index(row, col)][d]
+            board[row][col] = '.'    
+            
+        def place_next_numbers(row, col):
+            """
+            Call backtrack function in recursion
+            to continue to place numbers
+            till the moment we have a solution
+            """
+            # if we're in the last cell
+            # that means we have the solution
+            if col == N - 1 and row == N - 1:
+                nonlocal sudoku_solved
+                sudoku_solved = True
+            #if not yet    
             else:
-                False
-
-        def check_col(A, col):
-            list_col = [A[row][col] for row in range(len(A)) if A[row][col] != '.']
-            if len(list_col) == len(set(list_col)):
-                return True
-            else:
-                return False
-
-        def check_block(A, row, col):
-            nums_list = []
-            row_block = 3 * (row//3)
-            col_block = 3 * (col//3)
-            for row in range(row_block, row_block+2):
-                for col in range(col_block, col_block+2):
-                    if A[row][col] != '.':
-                        nums_list.append(A[row][col])
-            if len(nums_list) == len(set(nums_list)):
-                return True
-            else:
-                return False
-
-        def check_valid(A, row, col):
-            if check_row(A, row) and check_col(A, row) and check_block(A, row, col):
-                return True
-            else:
-                return False
-
-        # Explore all the options and see if sudoku is valid.
-        def helper(A, row, col, nums):
-            while(col < 9):
-                if A[row][col] != '.':
-                    col += 1
+                # if we're in the end of the row
+                # go to the next row
+                if col == N - 1:
+                    backtrack(row + 1, 0)
+                # go to the next column
                 else:
-                    ele = nums.pop(0)
-                    B = A[:]
-                    B[row] = B[row][:col]+ele+B[row][col+1:]
-                    if check_valid(B, row, col):
-                        A[row][col] = ele
-                        col += 1
-                    else:
-                        nums.append(ele)
-            return A
-
-        row = 0
-        col = 0
-        nums = list(range(1, 10))
-
-        row = 0 
-        col = 0
-        helper(A,row,col)
-
-        for row in range(len(A)):
-            col = 0
-            poss_nums = [str(x) for x in nums if str(x) not in A[row]]
-            A = helper(A, row, col, poss_nums)
+                    backtrack(row, col + 1)
+                
+                
+        def backtrack(row = 0, col = 0):
+            """
+            Backtracking
+            """
+            # if the cell is empty
+            if board[row][col] == '.':
+                # iterate over all numbers from 1 to 9
+                for d in range(1, 10):
+                    if could_place(d, row, col):
+                        place_number(d, row, col)
+                        place_next_numbers(row, col)
+                        # if sudoku is solved, there is no need to backtrack
+                        # since the single unique solution is promised
+                        if not sudoku_solved:
+                            remove_number(d, row, col)
+            else:
+                place_next_numbers(row, col)
+                    
+        # box size
+        n = 3
+        # row size
+        N = n * n
+        # lambda function to compute box index
+        box_index = lambda row, col: (row // n ) * n + col // n
+        
+        # init rows, columns and boxes
+        rows = [defaultdict(int) for i in range(N)]
+        columns = [defaultdict(int) for i in range(N)]
+        boxes = [defaultdict(int) for i in range(N)]
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] != '.': 
+                    d = int(board[i][j])
+                    place_number(d, i, j)
+        
+        sudoku_solved = False
+        backtrack()
 
 x = Solution()
 A = ["53..7....", "6..195...", ".98....6.", "8...6...3", "4..8.3..1", "7...2...6", ".6....28.", "...419..5", "....8..79"]
-x.solveSudoku(A)
+board = [[z for z in y] for y in A]
+x.solveSudoku(board)
+print(board)
